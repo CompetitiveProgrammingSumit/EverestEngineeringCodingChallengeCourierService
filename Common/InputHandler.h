c8a17165a2c009ed78146294793fcd8e82d5eb29
Sumit Makkar <makkarsumit1986@gmail.com>
2024-04-05 09:53:35 +0530

    Problem 1 Fixes: offer code can be NA now

--- Common/InputHandler.h
+++ Common/InputHandler.h
@@ -4,8 +4,6 @@
 #include <vector>
 #include "OptionalPackage.h"
 
-//std::unordered_set<std::string> OfferCodeDetails::offerCodeNamesSet = { "OFR001", "OFR002", "OFR003" };
-
 class InputHandler
 {
 	public:
--- Common/OfferCodeDetails.h
+++ Common/OfferCodeDetails.h
@@ -23,12 +23,12 @@ struct OfferCodeDetails
 
 	private:
 		std::unordered_map<std::string, Range> offerCodeDistanceRangeMap = {
-			{"OFR001", Range(0, 200)}, {"OFR002", Range(50, 150)}, {"OFR003", Range(50, 250)}
+			{"OFR001", Range(0, 200)}, {"OFR002", Range(50, 150)}, {"OFR003", Range(50, 250)}, {"NA", Range(0, 0)}
 		};
 		std::unordered_map<std::string, Range> offerCodeWeightRangeMap = {
-			{"OFR001", Range(70, 200)}, {"OFR002", Range(100, 250)}, {"OFR003", Range(10, 150)}
+			{"OFR001", Range(70, 200)}, {"OFR002", Range(100, 250)}, {"OFR003", Range(10, 150)}, {"NA", Range(0, 0)}
 		};
 		std::unordered_map<std::string, int> offerCodeDiscountMap = {
-			{"OFR001", 10}, {"OFR002", 7}, {"OFR003", 5}
+			{"OFR001", 10}, {"OFR002", 7}, {"OFR003", 5}, {"NA", 0}
 		};
 };
